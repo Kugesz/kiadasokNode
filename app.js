b036f2fe2eecf08d63d3938a9f2a252a94473ffa
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-25 19:27:01 +0200

    imported __dirname

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import bodyParser from "body-parser";
 
+import __dirname from "./util/rootpath.js";
 import login from "./routes/login.js";
 import register from "./routes/register.js";
 
@@ -11,8 +12,8 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/login", login);
 app.use("/register", register);
