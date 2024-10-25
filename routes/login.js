03062a18cac2c4afa009d41f35e553825ef9d9ec
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-25 10:17:15 +0200

    added the basic setup for routes

---
+++ routes/login.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import * as loginController from "../controllers/login.js";
+
+const router = express.Router();
+
+//Ide jonneg a connectionok get set stb.
+
+export default router;
---
+++ routes/register.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import * as registerController from "../controllers/register.js";
+
+const router = express.Router();
+
+//Ide jonneg a connectionok get set stb.
+
+export default router;
