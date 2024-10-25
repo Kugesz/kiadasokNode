138edc5ce7e94ee9e77c2d89fdf8b8199910526b
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-25 19:25:48 +0200

    added nemtom mit

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use("/login", login);
 app.use("/register", register);
 
