06a54135925802e047e491f5ede0b96a5aba40bf
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-25 10:16:56 +0200

    setup basic server

---
+++ app.js
@@ -0,0 +1,19 @@
+import path from "path";
+import express from "express";
+import bodyParser from "body-parser";
+
+import login from "./routes/login.js";
+import register from "./routes/register.js";
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use("/login", login);
+app.use("/register", register);
+
+app.listen(PORT, () => {
+  console.log(`server listens on port http://localhost:${PORT}`);
+});
