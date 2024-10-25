225d67dca59b94da344d8d2dcebf92d1ce1efc52
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-25 10:25:22 +0200

    changed names and created class

---
+++ models/expense.js
@@ -0,0 +1,10 @@
+class Expense {
+  constructor(id, category, amount, description) {
+    this.id = id;
+    this.category = category;
+    this.amount = amount;
+    this.description = description;
+  }
+}
+
+export default Expense;
