aae45673996c391a541127c0f056d0739c30bde0
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-28 01:58:22 +0100

    express.json

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
