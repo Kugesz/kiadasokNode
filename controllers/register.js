1687da60d781a2b252a4a0df9aef7515e0984310
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-28 02:09:27 +0100

    formated

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,6 @@
 import __dirname from "../util/rootpath.js";
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
 
 export const loadePage = (req, res, next) => {
   res.render("register.ejs", {
@@ -14,26 +14,35 @@ export const newRegister = (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if(!checkUsername(username)){
-    res.status(201).json({ message: 'User registered successfully', user: { username, password } });
-  }else{
-    res.status(400).json({message: "Ez a felhasználónév már foglalt!"})
+  if (!checkUsername(username)) {
+    res.status(201).json({
+      message: "User registered successfully",
+      user: { username, password },
+    });
+  } else {
+    res.status(400).json({ message: "Ez a felhasználónév már foglalt!" });
   }
-}
+};
 
 const checkUsername = (username) => {
-  fs.readFile(path.join(__dirname, "data", "users.json"), 'utf-8', (err, data) => {
-    if (err) {
-      console.error('Error reading JSON:', err);
-      return;
+  fs.readFile(
+    path.join(__dirname, "data", "users.json"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading JSON:", err);
+        return;
+      }
+
+      const jsonData = JSON.parse(data);
+
+      jsonData.forEach((user) => {
+        if (user.username == username) {
+          return false;
+        }
+      });
+
+      return true;
     }
-    
-    const jsonData = JSON.parse(data);
-  
-    jsonData.forEach(user => {
-      console.log(user.username)
-      if(user.username == username){return false;}
-    });
-    return true;
-  });
-}
+  );
+};
