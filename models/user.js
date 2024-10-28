1fb3817be8984cb58d060d65c21dedfe16da2f22
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-28 17:11:01 +0100

    added lot of things

--- models/user.js
+++ models/user.js
@@ -1,9 +1,47 @@
+import fs from "fs";
+import path from "path";
+import __dirname from "../util/rootpath.js";
+
+let users = [];
+
+const readFromTheFile = () => {
+  fs.readFile(
+    path.join(__dirname, "data", "users.json"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.error("Error reading JSON:", err);
+        return;
+      }
+
+      const jsonData = JSON.parse(data);
+
+      jsonData.forEach((user) => {
+        let newUser = new User(user.username, user.password);
+
+        users.push(newUser);
+      });
+    }
+  );
+};
+
 class User {
-  constructor(id, username, password) {
-    this.id = id;
+  constructor(username, password) {
     this.username = username;
     this.password = password;
     this.expenses = []; //Ide Expenses tipuso objectumok kerulnek
+  }
+
+  static load() {
+    readFromTheFile();
+  }
+
+  static checkLogin(){
+
+  }
+
+  static checkRegister(){
+    
   }
 }
 
