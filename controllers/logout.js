6bc435337503ce8ed215be9d6e5f2dd88c2fbf36
BorosBalazsLaszlo <c9borbal@vasvari.org>
2024-11-01 19:47:42 +0100

    logout added

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,9 +1,13 @@
-app.delete('/logout', (req, res) => {
-  req.session.destroy((err) => {
+import express from 'express';
+const router = express.Router();
+
+router.delete('/logout', (req, res) => {
+  req.logout((err) => {
     if (err) {
-      return res.redirect('/');
+      return res.status(500).json({ message: 'Error logging out' });
     }
-    res.clearCookie('session-id');
     res.redirect('/login');
   });
 });
+
+export default router;
