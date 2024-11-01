e1c78f34c7628356c592db039f600b30cb68bf6c
Kugesz <kovacsgergo.algyo@gmail.com>
2024-11-01 19:26:28 +0100

    empty line

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
-
 initializePassport(
   passport,
   async (username) => await User.findOne({ username: username }),
