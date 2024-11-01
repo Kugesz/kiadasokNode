264c1118eb9441e369771838d33b52d4caba6bd6
Kugesz <kovacsgergo.algyo@gmail.com>
2024-11-01 19:24:29 +0100

    added set bduget

--- controllers/index.js
+++ controllers/index.js
@@ -8,9 +8,7 @@ export const loadePage = (req, res, next) => {
   });
 };
 
-// Not working
 export const newExpense = async (req, res, next) => {
-  // username, expense
   try {
     const username = req.body.username;
     const expense = {
@@ -34,26 +32,36 @@ export const newExpense = async (req, res, next) => {
 };
 
 export const deleteExpense = async (req, res, next) => {
-    // username, expense
-    try {
-      const username = req.body.username;
-      const expense = {
-        description: req.body.description,
-        amount: req.body.amount,
-        category: req.body.category,
-      };
-      console.log(username, expense);
-  
-      await User.updateOne(
-        { username: username },
-        { $pull: { expenses: expense } }
-      );
-      res.status(201).json({ message: "Expense added successfully!" });
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "An error occurred while creating expense!" });
-    }
-  };
+  // username, expense
+  try {
+    const username = req.body.username;
+    const expense = {
+      description: req.body.description,
+      amount: req.body.amount,
+      category: req.body.category,
+    };
+    console.log(username, expense);
 
+    await User.updateOne(
+      { username: username },
+      { $pull: { expenses: expense } }
+    );
+    res.status(201).json({ message: "Expense added successfully!" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "An error occurred while creating expense!" });
+  }
+};
+
+export const setBudget = async (req, res, next) => {
+  const username = req.body.username;
+  const budget = req.body.budget;
+  try {
+    await User.updateOne({ username: username }, { $set: { budget: budget } });
+    res.status(201);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
