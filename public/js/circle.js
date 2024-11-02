cfd64eb5a3d3c87cb19f2551fab93dd58a8d63f5
Kugesz <kovacsgergo.algyo@gmail.com>
2024-11-02 22:52:53 +0100

    added controls for cirlce proggress

--- public/js/circle.js
+++ public/js/circle.js
@@ -1,5 +1,10 @@
-new CircleProgress('.progress', {
-	max: 100,
-	value: 60,
-	textFormat: 'percent',
-});
+function setProgress(percentage) {
+  const circle = document.querySelector(".progress-circle");
+  const radius = circle.r.baseVal.value;
+  const circumference = 2 * Math.PI * radius;
+
+  // Set the stroke-dasharray and stroke-dashoffset for the progress circle
+  circle.style.strokeDasharray = circumference;
+  circle.style.strokeDashoffset =
+    circumference - (percentage / 100) * circumference;
+}
