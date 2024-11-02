1d099e08788b6c39089cc8f12d78c2dde4fb266f
Kugesz <kovacsgergo.algyo@gmail.com>
2024-11-03 00:54:06 +0100

    added expense, bduget, spending

--- controllers/index.js
+++ controllers/index.js
@@ -2,16 +2,18 @@ import User from "../data/user.js";
 
 export const loadePage = async (req, res, next) => {
   const username = req.user.username;
-  const expenses = await User.findOne(
+  const data = await User.findOne(
     { username: username },
-    { expenses: 1, _id: 0 }
+    { expenses: 1, budget: 1, spending: 1, _id: 0 }
   );
   res.render("index.ejs", {
     pageTitle: "Kiadások",
     path: "/",
     editing: false,
     username: username,
-    expenses: expenses.expenses,
+    expenses: data.expenses,
+    budget: data.budget,
+    spending: data.spending,
   });
 };
 
