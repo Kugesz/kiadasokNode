5a3cdda95a8bebfd2a332679a950bb78c5fc1730
Kugesz <kovacsgergo.algyo@gmail.com>
2024-11-02 14:29:38 +0100

    created passwor dchange to data base

--- controllers/pw-change.js
+++ controllers/pw-change.js
@@ -1,14 +1,31 @@
 import bcrypt from "bcrypt";
+import User from "../data/user.js";
 
 // in body: password
-export const changePassword = (req, res, next) => {
-  const password = req.body.password;
+export const changePassword = async (req, res, next) => {
+  try {
+    const username = req.user.username;
+    const password = req.body.password;
 
-  // Itt adj hozzá kódot a jelszó titkosításához és mentéséhez az adatbázisban
-  // bcrypt.hash(password, 10, (err, hashedPassword) => {
-  //   if (err) return res.status(500).json({ success: false });
-  //   // Tovább a jelszó mentéséhez az adatbázisban
-  // });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  res.status(201).json({ success: true });
+    await User.findOne({ username: username }, { password: hashedPassword });
+
+    //! Jo kerdes hogy ez mukodik kesobbiekben teszt szukseges
+    req.logout();
+    res.redirect("/login");
+    res
+      .status(201)
+      .json({ message: "Your password change was successfull" })
+      .redirect("/login");
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({
+        message: "There was an error changin your password!",
+        error: err,
+      })
+      .redirect("/");
+  }
 };
